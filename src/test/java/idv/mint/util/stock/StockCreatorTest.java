5a39770bbf09ee29625f7737881f78786f61f9ea
yanswu <jquery132@gmail.com>
2018-01-23 14:55:19 +0800

    test2

---
+++ src/test/java/idv/mint/util/stock/StockCreatorTest.java
@@ -0,0 +1,47 @@
+package idv.mint.util.stock;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import idv.mint.bean.StockCategory;
+
+public class StockCreatorTest {
+
+    @Test
+    public void testCreateStockCategoryList() throws Exception {
+	
+	List<String> lines = new ArrayList<String>();
+	lines.add("1,6,TseCategoryName");
+	lines.add("2,7,OtcCategoryName");
+	
+	List<StockCategory> list = StockCreator.createStockCategoryList(lines);
+	StockCategory tseStockCategory = list.get(0);
+	StockCategory otcStockCategory = list.get(1);
+	
+	assertTrue(tseStockCategory.getMarketType().isTSE());
+	assertTrue(tseStockCategory.getSequence() == 6);
+	assertTrue(StringUtils.equals(tseStockCategory.getName(), "TseCategoryName"));
+
+	assertTrue(otcStockCategory.getMarketType().isOTC());
+	assertTrue(otcStockCategory.getSequence() == 7);
+	assertTrue(StringUtils.equals(otcStockCategory.getName(), "OtcCategoryName"));
+	
+	
+    }
+
+    @Test
+    public void testCreateStockList() throws Exception {
+	throw new RuntimeException("not yet implemented");
+    }
+
+    @Test
+    public void testCreateStockSheetEpsList() throws Exception {
+	throw new RuntimeException("not yet implemented");
+    }
+
+}
