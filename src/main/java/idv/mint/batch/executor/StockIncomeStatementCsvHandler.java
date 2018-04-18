aff3df067f1619723b3a7ab32f2c3c4f058d759a
yanswu <jquery132@gmail.com>
2018-04-18 17:43:03 +0800

    commit

---
+++ src/main/java/idv/mint/batch/executor/StockIncomeStatementCsvHandler.java
@@ -0,0 +1,49 @@
+package idv.mint.batch.executor;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import idv.mint.batch.TaskHandler;
+import idv.mint.bean.Stock;
+import idv.mint.support.PathSettings;
+import idv.mint.util.FileUtils;
+import idv.mint.util.crawl.Crawler;
+import idv.mint.util.stock.StockConverter;
+
+public class StockIncomeStatementCsvHandler extends TaskHandler {
+
+    @Override
+    public boolean execute(Map<String, Object> params) throws Exception {
+
+	// TSE
+	incomeStatementCsvWriter(PathSettings.STOCK_TSE_CSV, PathSettings.STOCK_INCOMESTATEMENT_TSE_CSV);
+
+	incomeStatementCsvWriter(PathSettings.STOCK_OTC_CSV, PathSettings.STOCK_INCOMESTATEMENT_OTC_CSV);
+
+	return false;
+    }
+
+    private void incomeStatementCsvWriter(PathSettings stockInPath, PathSettings stockOutPath) throws IOException {
+
+	List<String> lines = Files.readAllLines(stockInPath.getPath(), StandardCharsets.UTF_8);
+
+	List<Stock> stockList = StockConverter.convertStockList(lines);
+
+	Crawler crawler = Crawler.createWebCrawler();
+
+	Path writePath = stockOutPath.getPath();
+
+	FileUtils.cleanFile(writePath);
+
+	for (Stock stock : stockList) {
+	    // pattern : stockCode, rocYear,netIncome
+	    List<String> incomeStatementLines = crawler.getIncomeStatementLines(stock.getStockCode());
+	    FileUtils.writeFileAppend(writePath, incomeStatementLines);
+	}
+    }
+
+}
